19ba9994aab1470713337497b158ba701d92e3bb
lfremy <laurafremy19@gmail.com>
2022-05-12 18:33:19 +0200

    my first project with nuxt JS understanding the logic

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <main class="bg-red-500 text-center">
+    <h1>TODO</h1>
+    <p>Crée une liste de tâches</p>
+
+    <div >
+      <input 
+      class="bg-yellow-200"
+        type="text" 
+        v-model="newTask" 
+        placeholder="Add a new task" 
+        @keypress.enter="addTask" />
+      <button @click="addTask">Add</button>
+    </div>
+
+    <div class="tasks bg-gray-500 ">
+      <Task class='flex justify-around'
+        v-for="(task, i) in $store.state.tasks"
+        :key="i"
+        :task="task" />
+    </div>
+
+  </main>
+</template>
+
+<script>
+export default {
+  data (){
+    return {
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask () {
+      if (this.newTask) {
+        this.$store.commit('ADD_TASK', this.newTask);
+        this.newTask = '';
+      }
+    }
+  }
+}
+</script>
